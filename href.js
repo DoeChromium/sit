b441012b72a3eb6e3995774a74d9096816747e5f
DoeChromium <142622054+DoeChromium@users.noreply.github.com>
2025-04-20 20:53:12 -0700

    v3 TEST

---
+++ href.js
@@ -0,0 +1,19 @@
+function a1() {
+    location.href=('https://docs.google.com/forms/d/e/1FAIpQLSfRCxJ9Ze56ej9-DBKNMAIvc-wO2iVONtDEPFUEInntmv4vBA/viewform')
+}
+
+function b1() {
+    location.href=('https://www.instagram.com/sit_orhs/')
+}
+
+function b2() {
+    location.href=('https://discord.gg/n4ngUWPA5v')
+}
+
+function c1() {
+    location.href=('https://doechromium.github.io/sit/')
+}
+
+function d1() {
+    location.href=('https://github.com/DoeChromium/sit')
+}
---
+++ scripts.js
@@ -0,0 +1,11 @@
+function info() {
+    alert('Version 3.0 STABLE\nMobile Support: n/a\nWebsite Revision Version: 3\n\nCurrent URL: https://doechromium.github.io/sit/\nNon-responsive, no forms or sql\n\nContact Isaac for changes')
+}
+
+function developer() {
+    alert('Developer error codes\n\n0x01 · Gmail mailto: no longer works\n0x02 · Javascript disabled\n\nDeveloper attention needed\n\nCSS revision\nLogo revision\nMobile support\nRecoloring/Sat fix')
+}
+
+function gmailSorry() {
+    alert('Sorry, the link to bring you to Gmail doesn\'t work anymore!\n\nPlease contact us through orhs.sit@gmail.com\n\n0x01')
+}
